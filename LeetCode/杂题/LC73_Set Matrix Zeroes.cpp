3dfdc0081c4bdcc36e4103a6faf99b7ddaaf47ad
岩 曹 <935524537@qq.com>
2021-03-21 09:34:54 +0800

    打卡150，73

---
+++ LeetCode/杂题/LC73_Set Matrix Zeroes.cpp
@@ -0,0 +1,71 @@
+﻿/*
+题目描述：LeetCode 73
+
+给定一个 m x n 的矩阵，如果一个元素为 0，则将其所在行和列的所有元素都设为 0。请使用原地算法。
+
+输入:
+[
+  [1,1,1],
+  [1,0,1],
+  [1,1,1]
+]
+输出:
+[
+  [1,0,1],
+  [0,0,0],
+  [1,0,1]
+]
+*/
+class Solution {
+public:
+    void setrow(vector<vector<int>>& m, int num) {
+        for (int j = 0; j < m[0].size(); j++) {
+            m[num][j] = 0;
+        }
+    }
+    void setcol(vector<vector<int>>& m, int num) {
+        for (int j = 0; j < m.size(); j++) {
+            m[j][num] = 0;
+        }
+    }
+    void setZeroes(vector<vector<int>>& m) {
+        bool row = false, col = false;
+        for (int i = 0; i < m.size(); i++) {
+            if (m[i][0] == 0) {
+                col = true;
+                break;
+            }
+        }
+        for (int i = 0; i < m[0].size(); i++) {
+            if (m[0][i] == 0) {
+                row = true;
+                break;
+            }
+        }
+        for (int i = 1; i < m.size(); i++) {
+            for (int j = 1; j < m[0].size(); j++) {
+                if (m[i][j] == 0) {
+                    m[i][0] = 0;
+                    m[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m[0].size(); i++) {
+            if (m[0][i] == 0) {
+                setcol(m, i);
+            }
+        }
+        for (int i = 1; i < m.size(); i++) {
+            if (m[i][0] == 0) {
+                setrow(m, i);
+            }
+        }
+        if (row == true) {
+            setrow(m, 0);
+        }
+        if (col == true) {
+            setcol(m, 0);
+        }
+
+    }
+};
---
+++ LeetCode/栈/LC150_Reverse Polish Notation.cpp
@@ -0,0 +1,40 @@
+﻿/*
+题目描述：LeetCode 150
+
+根据 逆波兰表示法，求表达式的值。
+有效的算符包括 +、-、*、/ 。每个运算对象可以是整数，也可以是另一个逆波兰表达式。
+
+输入：tokens = ["2","1","+","3","*"]
+输出：9
+解释：该算式转化为常见的中缀算术表达式为：((2 + 1) * 3) = 9
+*/
+const int maxn = 1e5 + 50;
+int st[maxn], idx;
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        idx = -1;
+        for (const auto& s : tokens) {
+            if (s == "+" or s == "-" or s == "*" or s == "/") {
+                int a = st[idx--];
+                int b = st[idx--];
+                if (s == "+") {
+                    st[++idx] = a + b;
+                }
+                else if (s == "-") {
+                    st[++idx] = b - a;
+                }
+                else if (s == "*") {
+                    st[++idx] = a * b;
+                }
+                else {
+                    st[++idx] = b / a;
+                }
+            }
+            else {
+                st[++idx] = stoi(s);
+            }
+        }
+        return st[idx];
+    }
+};
