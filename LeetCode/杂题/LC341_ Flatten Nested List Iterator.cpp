8fab061b31f2913421a7b13750d4b119b6647ccb
岩 曹 <935524537@qq.com>
2021-03-23 09:06:53 +0800

    打卡341

---
+++ LeetCode/杂题/LC341_ Flatten Nested List Iterator.cpp
@@ -0,0 +1,45 @@
+﻿/*
+题目描述：LeetCode 341
+
+给你一个嵌套的整型列表。请你设计一个迭代器，使其能够遍历这个整型列表中的所有整数。
+
+列表中的每一项或者为一个整数，或者是另一个列表。其中列表的元素也可能是整数或是其他列表。
+
+
+输入: [[1,1],2,[1,1]]
+输出: [1,1,2,1,1]
+解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,1,2,1,1]。
+
+*/
+class NestedIterator {
+private:
+    // pair 中存储的是列表的当前遍历位置，以及一个尾后迭代器用于判断是否遍历到了列表末尾
+    stack<pair<vector<NestedInteger>::iterator, vector<NestedInteger>::iterator>> stk;
+
+public:
+    NestedIterator(vector<NestedInteger>& nestedList) {
+        stk.emplace(nestedList.begin(), nestedList.end());
+    }
+
+    int next() {
+        // 由于保证调用 next 之前会调用 hasNext，直接返回栈顶列表的当前元素，然后迭代器指向下一个元素
+        return stk.top().first++->getInteger();
+    }
+
+    bool hasNext() {
+        while (!stk.empty()) {
+            auto& p = stk.top();
+            if (p.first == p.second) { // 遍历到当前列表末尾，出栈
+                stk.pop();
+                continue;
+            }
+            if (p.first->isInteger()) {
+                return true;
+            }
+            // 若当前元素为列表，则将其入栈，且迭代器指向下一个元素
+            auto& lst = p.first++->getList();
+            stk.emplace(lst.begin(), lst.end());
+        }
+        return false;
+    }
+};
