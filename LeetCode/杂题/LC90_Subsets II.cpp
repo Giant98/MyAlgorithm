d28b91c48b4024b8074ee1baee78dc2be8b85d5a
岩 曹 <935524537@qq.com>
2021-03-31 09:52:52 +0800

    打卡90，修改readme

---
+++ LeetCode/杂题/LC90_Subsets II.cpp
@@ -0,0 +1,36 @@
+/*
+题目描述：LeetCode 90
+
+给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。
+解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
+
+输入：nums = [1,2,2]
+输出：[[],[1],[1,2],[1,2,2],[2],[2,2]]
+*/
+class Solution {
+public:
+    vector<int> t;
+    vector<vector<int>> ans;
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for (int mask = 0; mask < (1 << n); ++mask) {
+            t.clear();
+            bool flag = true;
+            for (int i = 0; i < n; ++i) {
+                if (mask & (1 << i)) {
+                    if (i > 0 && (mask >> (i - 1) & 1) == 0 && nums[i] == nums[i - 1]) {
+                        flag = false;
+                        break;
+                    }
+                    t.push_back(nums[i]);
+                }
+            }
+            if (flag) {
+                ans.push_back(t);
+            }
+        }
+        return ans;
+    }
+};
