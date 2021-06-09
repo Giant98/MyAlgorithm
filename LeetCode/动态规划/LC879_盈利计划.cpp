404a78f9ab061747258fa0dde111ceb3b8f46042
Yan Cao <935524537@qq.com>
2021-06-09 09:20:11 +0800

    打卡879

---
+++ LeetCode/动态规划/LC879_盈利计划.cpp
@@ -0,0 +1,32 @@
+﻿/*
+题目描述：LeetCode 879
+
+集团里有 n 名员工，他们可以完成各种各样的工作创造利润。
+第 i 种工作会产生 profit[i] 的利润，它要求 group[i] 名成员共同参与。如果成员参与了其中一项工作，就不能参与另一项工作。
+工作的任何至少产生 minProfit 利润的子集称为 盈利计划 。并且工作的成员总数最多为 n 。
+有多少种计划可以选择？因为答案很大，所以 返回结果模 10^9 + 7 的值。
+
+输入：n = 10, minProfit = 5, group = [2,3,5], profit = [6,7,8]
+输出：7
+解释：至少产生 5 的利润，只要完成其中一种工作就行，所以该集团可以完成任何工作。
+有 7 种可能的计划：(0)，(1)，(2)，(0,1)，(0,2)，(1,2)，以及 (0,1,2) 。
+*/
+class Solution {
+public:
+    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
+        vector<vector<int>> dp(n + 1, vector<int>(minProfit + 1));
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        int len = group.size(), MOD = (int)1e9 + 7;
+        for (int i = 1; i <= len; i++) {
+            int members = group[i - 1], earn = profit[i - 1];
+            for (int j = n; j >= members; j--) {
+                for (int k = minProfit; k >= 0; k--) {
+                    dp[j][k] = (dp[j][k] + dp[j - members][max(0, k - earn)]) % MOD;
+                }
+            }
+        }
+        return dp[n][minProfit];
+    }
+};
