2f8100be7a4311c1407a90db50ae6ea28a47df76
Yan Cao <935524537@qq.com>
2021-08-26 09:35:08 +0800

    打卡881

---
+++ LeetCode/贪心/LC881_救生艇.cpp
@@ -0,0 +1,30 @@
+﻿/*
+题目描述：LeetCode 881
+
+第 i 个人的体重为 people[i]，每艘船可以承载的最大重量为 limit。
+每艘船最多可同时载两人，但条件是这些人的重量之和最多为 limit。
+返回载到每一个人所需的最小船数。(保证每个人都能被船载)。
+
+输入：people = [1,2], limit = 3
+输出：1
+解释：1 艘船载 (1, 2)
+*/
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int ans = 0;
+        sort(people.begin(), people.end());
+        int light = 0, heavy = people.size() - 1;
+        while (light <= heavy) {
+            if (people[light] + people[heavy] > limit) {
+                --heavy;
+            }
+            else {
+                ++light;
+                --heavy;
+            }
+            ++ans;
+        }
+        return ans;
+    }
+};
--- Readme.md
+++ Readme.md
@@ -134,6 +134,7 @@
 | 1646 | [获取生成数组中的最大值](https://leetcode-cn.com/problems/get-maximum-in-generated-array/) | Easy | 无 | 模拟 |
 | 787 | [K 站中转内最便宜的航班](https://leetcode-cn.com/problems/cheapest-flights-within-k-stops/) | Medium | Dijkstra算法 | 经典⭐ |
 | 797 | [所有可能的路径](https://leetcode-cn.com/problems/all-paths-from-source-to-target/) | Medium | 栈+深搜 | 经典⭐ |
+| 881 | [救生艇](https://leetcode-cn.com/problems/boats-to-save-people/) | Medium | 贪心 | 经典⭐据说是腾讯面试题 |
 
 
 
